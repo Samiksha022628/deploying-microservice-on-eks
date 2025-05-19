fdeff3ccd8b6cdeb3f1042a76fecadda607e1b75
Samiksha022628 <samiksha022628@gmail.com>
2025-05-19 15:56:45 +0530

    change 1

--- bin/deploying-microservice-on-eks.ts
+++ bin/deploying-microservice-on-eks.ts
@@ -14,7 +14,7 @@ new DeployingMicoserviceOnEksStack(app, 'DeployingMicoserviceOnEksStack',{
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '654654582602', region: 'us-east-1' },
+  env: { account: '339713126438', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
