70d7464a8cf5ebaaed4f499989e0c2c043f06939
Samiksha022628 <samiksha022628@gmail.com>
2025-05-19 14:52:31 +0530

    change new stacks 3

--- bin/deploying-microservice-on-eks.ts
+++ bin/deploying-microservice-on-eks.ts
@@ -7,11 +7,15 @@ const app = new cdk.App();
 const targetStack = app.node.tryGetContext('stack');
 const env = { account: '654654582602', region: 'us-east-1' };
 
-if (targetStack === 'iam-vpc') {
-  new IamAndVpcStack(app, 'IamAndVpcStack', { env });}
-
-if (targetStack === 'eks-cluster') {
-  new EksClusterStack(app, 'EksClusterStack', { env });}
+if (!targetStack) {
+  console.log('No context provided. Skipping stack instantiation. This is safe for "cdk bootstrap".');
+} else if (targetStack === 'iam-vpc') {
+  new IamAndVpcStack(app, 'IamAndVpcStack', { env });
+} else if (targetStack === 'eks-cluster') {
+  new EksClusterStack(app, 'EksClusterStack', { env });
+} else {
+  throw new Error(`Unknown context stack "${targetStack}". Expected "iam-vpc" or "eks-cluster".`);
+}
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
