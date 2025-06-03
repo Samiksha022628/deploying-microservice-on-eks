aa170fc0c3e3329b98ca9dd175e132f23e05bc14
meghana-kalburgi <meghs.kalburgi@gmail.com>
2025-06-03 19:58:46 +0530

    Testing4

--- lib/deploying-microservice-on-eks.ts
+++ lib/deploying-microservice-on-eks.ts
@@ -11,192 +11,171 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 export class DeployingMicoserviceOnEksStack extends cdk.Stack{
   constructor(scope:Construct, id:string, props?:cdk.StackProps) {super(scope,id,props);
 
-   const envconfigs = this.node.tryGetContext('envconfigs');
+       
+    const envconfigs = this.node.tryGetContext('envconfigs');
 
-const iamroleforcluster = new iam.Role(this, 'EksAdminRole', {
-  assumedBy: new iam.AccountRootPrincipal(),
-});
+    const iamroleforcluster = new iam.Role(this, 'EksAdminRole', {
+      assumedBy: new iam.AccountRootPrincipal(),
+    });
 
-const vpc = new ec2.Vpc(this, 'vpc', {
-  natGateways: 1,
-  subnetConfiguration: [
-    {
-      name: 'PrivateSubnet',
-      subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-      cidrMask: 24,
-    },
-    {
-      name: 'PublicSubnet',
-      subnetType: ec2.SubnetType.PUBLIC,
-      cidrMask: 24,
-    },
-  ],
-});
-
-const cluster = new eks.Cluster(this, 'EksCluster', {
-  clusterName: 'EksCluster',
-  defaultCapacity: 0,
-  vpc,
-  version: eks.KubernetesVersion.V1_28,
-  kubectlLayer: new KubectlV28Layer(this, 'kubectl'),
-  vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
-  mastersRole: iamroleforcluster,
-});
-
-const nodegroup = cluster.addNodegroupCapacity('NodeGroup', {
-  desiredSize: 2,
-  instanceTypes: [new ec2.InstanceType('t3.medium')],
-  remoteAccess: {
-    sshKeyName: 'demo',
-  },
-});
-
-nodegroup.role.addManagedPolicy(
-  iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
-);
-
-cluster.awsAuth.addRoleMapping(nodegroup.role, {
-  username: 'system:node:{{EC2PrivateDNSName}}',
-  groups: ['system:bootstrappers', 'system:nodes', 'system:masters'],
-});
-
-cluster.addHelmChart('MetricsServer', {
-  chart: 'metrics-server',
-  repository: 'https://kubernetes-sigs.github.io/metrics-server/',
-  release: 'metrics-server',
-  namespace: 'kube-system',
-  values: {
-    args: [
-      '--kubelet-insecure-tls',
-      '--kubelet-preferred-address-types=InternalIP,Hostname,ExternalIP',
-    ],
-  },
-});
-
-const namespaceManifestPath = path.join(
-  __dirname,
-  '..',
-  'manifests',
-  'namespace-cloudwatch.yaml'
-);
-const namespaceManifestContent = fs.readFileSync(namespaceManifestPath, 'utf8');
-const namespaceDocs = yaml.parseAllDocuments(namespaceManifestContent);
-const namespaceResources = namespaceDocs.map((doc) => doc.toJSON()).filter(Boolean);
-const cloudwatchNamespace = cluster.addManifest(
-  'CloudWatchNamespace',
-  ...namespaceResources
-);
-
-const conditionJson = new cdk.CfnJson(this, 'OIDCCondition', {
-  value: {
-    [`${cluster.openIdConnectProvider.openIdConnectProviderIssuer}:sub`]:
-      'system:serviceaccount:amazon-cloudwatch:fluent-bit',
-  },
-});
-
-const fluentBitSaRole = new iam.Role(this, 'FluentBitIRSA', {
-  assumedBy: new iam.WebIdentityPrincipal(
-    cluster.openIdConnectProvider.openIdConnectProviderArn,
-    {
-      StringEquals: conditionJson,
-    }
-  ),
-});
-
-fluentBitSaRole.addToPrincipalPolicy(
-  new iam.PolicyStatement({
-    effect: iam.Effect.ALLOW,
-    actions: [
-      'logs:CreateLogGroup',
-      'logs:CreateLogStream',
-      'logs:PutLogEvents',
-      'logs:DescribeLogStreams',
-    ],
-    resources: [`arn:aws:logs:${this.region}:${this.account}:log-group:/eks/*`],
-  })
-);
-
-const manifestsDir = 'manifests';
-const files = [
-  'namespace.yaml',
-  'rolebinding.yaml',
-  'configMap-secret.yaml',
-  'deployment.yaml',
-  'HPA.yaml',
-  'job.yaml',
-];
-
-let previousHelmChart: eks.HelmChart | undefined = undefined;
-
-for (const envName of Object.keys(envconfigs)) {
-  const config = envconfigs[envName];
-
-  const placeholders: Record<string, string> = {
-    '{{ENV}}': envName,
-    '{{APP_VERSION}}': config.appVersion || '1.0.0',
-    '{{REPLICA_COUNT}}': (config.replicaCount || 1).toString(),
-    '{{REQUEST_CPU}}': config.requestCpu || '100m',
-    '{{LIMIT_CPU}}': config.limitCpu || '200m',
-    '{{FEATURE_FLAG}}': config.featureFlag === undefined ? 'false' : config.featureFlag.toString(),
-  };
-
-  const replacePlaceholders = (content: string) => {
-    for (const [key, value] of Object.entries(placeholders)) {
-      content = content.replace(new RegExp(key, 'g'), value);
-    }
-    return content;
-  };
-
-  const allResources = files.flatMap((file) => {
-    const content = replacePlaceholders(
-      fs.readFileSync(path.join(manifestsDir, file), 'utf8')
-    );
-    return yaml.parseAllDocuments(content).map((doc) => doc.toJSON()).filter(Boolean);
-  });
-
-  const namespaceResources = allResources.filter((res) => res.kind === 'Namespace');
-  const otherResources = allResources.filter((res) => res.kind !== 'Namespace');
-
-  const namespaceManifest = cluster.addManifest(`NamespaceManifest-${envName}`, ...namespaceResources);
-  const appManifest = cluster.addManifest(`AppManifests-${envName}`, ...otherResources);
-
-  appManifest.node.addDependency(namespaceManifest);
-
-  const fluentBit = cluster.addHelmChart(`FluentBit-${envName}`, {
-    chart: 'aws-for-fluent-bit',
-    repository: 'https://aws.github.io/eks-charts',
-    release: `fluent-bit-${envName}`,
-    namespace: 'amazon-cloudwatch',
-    createNamespace: false,
-    values: {
-      serviceAccount: {
-        create: false,
-        name: 'fluent-bit',
-        annotations: {
-          'eks.amazonaws.com/role-arn': fluentBitSaRole.roleArn,
-        },
-      },
-      cloudWatch: {
-        enabled: true,
-        logGroupName: `/eks/${envName}/app-logs`,
-        region: this.region,
-        autoCreateGroup: true,
-      },
-      tolerations: [
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      natGateways: 1,
+      subnetConfiguration: [
         {
-          key: 'node-role.kubernetes.io/control-plane',
-          effect: 'NoSchedule',
+          name: 'PrivateSubnet',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 24,
+        },
+        {
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
         },
       ],
-    },
-  });
+    });
 
-  fluentBit.node.addDependency(cloudwatchNamespace);
-  if (previousHelmChart) {
-    fluentBit.node.addDependency(previousHelmChart);
-  }
-  previousHelmChart = fluentBit;
-}
+    const cluster = new eks.Cluster(this, 'EksCluster', {
+      clusterName: 'EksCluster',
+      defaultCapacity: 0,
+      vpc,
+      version: eks.KubernetesVersion.V1_28,
+      kubectlLayer: new KubectlV28Layer(this, 'kubectl'),
+      vpcSubnets: [{ subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+      mastersRole: iamroleforcluster,
+    });
 
+    const nodegroup = cluster.addNodegroupCapacity('NodeGroup', {
+      desiredSize: 2,
+      instanceTypes: [new ec2.InstanceType('t3.medium')],
+      remoteAccess: {
+        sshKeyName: 'demo',
+      },
+    });
+
+    nodegroup.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
+    );
+
+    cluster.awsAuth.addRoleMapping(nodegroup.role, {
+      username: 'system:node:{{EC2PrivateDNSName}}',
+      groups: ['system:bootstrappers', 'system:nodes', 'system:masters'],
+    });
+
+    cluster.addHelmChart('MetricsServer', {
+      chart: 'metrics-server',
+      repository: 'https://kubernetes-sigs.github.io/metrics-server/',
+      release: 'metrics-server',
+      namespace: 'kube-system',
+      values: {
+        args: [
+          '--kubelet-insecure-tls',
+          '--kubelet-preferred-address-types=InternalIP,Hostname,ExternalIP',
+        ],
+      },
+    });
+
+    const conditionJson = new cdk.CfnJson(this, 'OIDCCondition', {
+      value: {
+        [`${cluster.openIdConnectProvider.openIdConnectProviderIssuer}:sub`]:
+          'system:serviceaccount:amazon-cloudwatch:fluent-bit',
+      },
+    });
+
+    const fluentBitSaRole = new iam.Role(this, 'FluentBitIRSA', {
+      assumedBy: new iam.WebIdentityPrincipal(
+        cluster.openIdConnectProvider.openIdConnectProviderArn,
+        {
+          StringEquals: conditionJson,
+        }
+      ),
+    });
+
+    fluentBitSaRole.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          'logs:CreateLogGroup',
+          'logs:CreateLogStream',
+          'logs:PutLogEvents',
+          'logs:DescribeLogStreams',
+        ],
+        resources: [`arn:aws:logs:${this.region}:${this.account}:log-group:/eks/*`],
+      })
+    );
+
+    const valuesYamlPath = path.join(__dirname, '..', 'manifests', 'values.yaml');
+    const valuesYamlContent = fs.readFileSync(valuesYamlPath, 'utf8');
+    const values = yaml.parse(valuesYamlContent);
+
+    const manifestsDir = 'manifests';
+    const files = [
+      'namespace.yaml',
+      'rolebinding.yaml',
+      'configMap-secret.yaml',
+      'deployment.yaml',
+      'HPA.yaml',
+      'job.yaml',
+    ];
+
+    let previousHelmChart: eks.HelmChart | undefined = undefined;
+
+    for (const envName of Object.keys(envconfigs)) {
+      const config = envconfigs[envName];
+
+      const placeholders: Record<string, string> = {
+        '{{ENV}}': envName,
+        '{{APP_VERSION}}': config.appVersion || '1.0.0',
+        '{{REPLICA_COUNT}}': (config.replicaCount || 1).toString(),
+        '{{REQUEST_CPU}}': config.requestCpu || '100m',
+        '{{LIMIT_CPU}}': config.limitCpu || '200m',
+        '{{FEATURE_FLAG}}': config.featureFlag === undefined ? 'false' : config.featureFlag.toString(),
+        '{{LOG_GROUP_NAME}}': `/eks/${envName}/app-logs`,  // Dynamic log group name
+      };
+
+      const replacePlaceholders = (content: string) => {
+        for (const [key, value] of Object.entries(placeholders)) {
+          content = content.replace(new RegExp(key, 'g'), value);
+        }
+        return content;
+      };
+
+      const allResources = files.flatMap((file) => {
+        const content = replacePlaceholders(
+          fs.readFileSync(path.join(manifestsDir, file), 'utf8')
+        );
+        return yaml.parseAllDocuments(content).map((doc) => doc.toJSON()).filter(Boolean);
+      });
+
+      const namespaceResources = allResources.filter((res) => res.kind === 'Namespace');
+      const otherResources = allResources.filter((res) => res.kind !== 'Namespace');
+
+      const namespaceManifest = cluster.addManifest(`NamespaceManifest-${envName}`, ...namespaceResources);
+      const appManifest = cluster.addManifest(`AppManifests-${envName}`, ...otherResources);
+
+      appManifest.node.addDependency(namespaceManifest);
+
+      const fluentBit = cluster.addHelmChart(`FluentBit-${envName}`, {
+        chart: 'aws-for-fluent-bit',
+        repository: 'https://aws.github.io/eks-charts',
+        release: `fluent-bit-${envName}`,
+        namespace: 'amazon-cloudwatch',
+        createNamespace: false,
+        values: {
+          ...values, // Inject parsed values.yaml content
+          serviceAccount: {
+            name: 'fluent-bit',
+            annotations: {
+              'eks.amazonaws.com/role-arn': fluentBitSaRole.roleArn,
+            },
+          },
+        },
+      });
+
+      fluentBit.node.addDependency(namespaceManifest);
+      if (previousHelmChart) {
+        fluentBit.node.addDependency(previousHelmChart);
+      }
+      previousHelmChart = fluentBit;
+    }
 }}
 
